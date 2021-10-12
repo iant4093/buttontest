a14358732177b407a3284c42d51fa041113999ac
Ian Truong <ianthegreat60@gmail.com>
2021-10-12 14:50:42 -0700

    Initial Commit

---
+++ financeapp/financeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeappApp.swift
+//  financeapp
+//
+//  Created by Ian Truong on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct financeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
