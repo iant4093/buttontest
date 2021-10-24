c3e61e6dd518638f30028e1621145072cfcdb806
Ian Truong <ianthegreat60@gmail.com>
2021-10-24 15:26:00 -0700

    pee

--- financeapp/ContentView.swift
+++ financeapp/ContentView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, harry!")
-            .padding()
+        
+        VStack{
+            
+            Text("Welcome to our poopy app!")
+                .fontWeight(.bold)
+                .padding() // add the padding
+                .background(Color.red) // change background to blue
+                .font(.largeTitle) // change font type
+                .cornerRadius(100)
+                .padding(10)
+               // .border(Color.red , width: 5)
+                .foregroundColor(Color.black)
+                
+                .overlay(RoundedRectangle(cornerRadius: 100)
+                            .stroke(lineWidth: 2 )
+                )
+            Button(action:  {
+                // what to perform
+                print("Click for financial help tapped!")
+            }) {
+                // how the button looks like
+                HStack{
+                    Image(systemName: "square.and.pencil")
+                        .font(.title)
+                    Text("Click for financial help!")
+                        .fontWeight(.semibold)
+                        .font(.title) // change font type
+                        
+                
+                }
+                .padding()
+
+                .foregroundColor(.red) //change foreground color to red
+                .border(Color.gray, width: 3 )
+            
+        
+            }
+        }
     }
 }
 
@@ -19,3 +55,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
